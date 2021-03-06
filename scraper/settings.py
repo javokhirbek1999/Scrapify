b8597447311ae84e43beac0e8d1546d513467f57
Javokhirbek <khaydaraliev99@gmail.com>
2021-03-06 15:30:42 +0100

    Added Allowed Hosts

--- scraper/settings.py
+++ scraper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')o#cskz#e7v#ou$y)a99$i74eo7ug!ho=rvq29f75u&r89jr)a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'scrapifylist.herokuapp.com']
 
 
 # Application definition
